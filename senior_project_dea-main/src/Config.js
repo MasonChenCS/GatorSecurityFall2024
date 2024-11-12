ffd421eee81c82bd81d64981c558afca84aa599c
Rohit Mittal <51212933+rohitmitt@users.noreply.github.com>
2024-11-12 15:56:01 -0500

    Update Config.js

--- senior_project_dea-main/src/Config.js
+++ senior_project_dea-main/src/Config.js
@@ -1,4 +1,6 @@
 //To change the address of the machine hosting the backend, modify below.
+// i.e. "SERVER_ADDRESS": "http://127.0.0.1:5000", where 127.0.0.1 is the public IPv4 of the hosting machine. 
+
 function GetConfig() {
     return {
         "SERVER_ADDRESS": "http://localhost:5000"
